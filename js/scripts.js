a8999a87b6aa5835b25818f5b4d9218e1a0d6f4a
Brandon Vasquez <thatsjaskett@gmail.com>
2022-12-20 16:29:22 +0100

    Creazione event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,44 @@
-// Dichiaro le variabili Km ed Età
-let distance = parseInt(prompt("Quanti Kilometri devi percorrere?"));
-const age = parseInt(prompt("Quanti anni hai?"));
-console.log(`L'utente ha ${age} anni`);
-console.log(`L'utente deve percorrere ${distance}Km`);
+let submit = document.getElementById('generate_ticket');
 
-if(isNaN(age) || isNaN(distance)) {
-    alert("Il valore inserito non è un numero! Per favore, inserire correttamente i valori richiesti");
-    location.reload();
-}
+submit.addEventListener('click',
+    function() {
+        let name_passenger = document.getElementById('name').value;
+        console.log(`L'utente si chiama ${name_passenger}`);
+        let distance = document.getElementById('distance').value;
+        console.log(`L'utente deve percorrere ${distance}Km`);
+        let age = document.getElementById('age').value;
+        console.log(`L'utente rientra nella categoria sconto ${age}`);
+    }    
+);
 
-// Calcolo prezzi biglietti
-const km_price = 0.21;
-const full_price_ticket = distance * km_price;
-const ticket = full_price_ticket.toFixed(2);
-console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
+// if(isNaN(age) || isNaN(distance)) {
+//     alert("Il valore inserito non è un numero! Per favore, inserire correttamente i valori richiesti");
+//     location.reload();
+// }
 
-// Calcolo sconto Under18
-let ticket_under = full_price_ticket * 0.80;
-let ticket_d_under = ticket_under.toFixed(2);
-console.log(`Il prezzo del biglietto per Under 18 è: ${ticket_d_under}€`);
+// // Calcolo prezzi biglietti
+// const km_price = 0.21;
+// const full_price_ticket = distance * km_price;
+// const ticket = full_price_ticket.toFixed(2);
+// console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
 
-// Calcolo sconto Over65
-let ticket_over = full_price_ticket * 0.60;
-let ticket_d_over = ticket_over.toFixed(2);
-console.log(`Il prezzo del biglietto per Over 65 è: ${ticket_d_over}€`);
+// // Calcolo sconto Under18
+// let ticket_under = full_price_ticket * 0.80;
+// let ticket_d_under = ticket_under.toFixed(2);
+// console.log(`Il prezzo del biglietto per Under 18 è: ${ticket_d_under}€`);
 
-// Output prezzo biglietto del treno con sconti
-if(age < 18) {
-    alert(`Complimenti! Hai diritto allo sconto per Under 18. Il prezzo del biglietto è di ${ticket_d_under}€`);
-}
-else if(age > 65) {
-    alert(`Complimenti! Hai diritto allo sconto per Over 65. Il prezzo del biglietto è di ${ticket_d_over}€`);
-}
-else {
-    alert(`Il prezzo del biglietto è di ${ticket}€`);
-}
+// // Calcolo sconto Over65
+// let ticket_over = full_price_ticket * 0.60;
+// let ticket_d_over = ticket_over.toFixed(2);
+// console.log(`Il prezzo del biglietto per Over 65 è: ${ticket_d_over}€`);
+
+// // Output prezzo biglietto del treno con sconti
+// if(age < 18) {
+//     alert(`Complimenti! Hai diritto allo sconto per Under 18. Il prezzo del biglietto è di ${ticket_d_under}€`);
+// }
+// else if(age > 65) {
+//     alert(`Complimenti! Hai diritto allo sconto per Over 65. Il prezzo del biglietto è di ${ticket_d_over}€`);
+// }
+// else {
+//     alert(`Il prezzo del biglietto è di ${ticket}€`);
+// }
