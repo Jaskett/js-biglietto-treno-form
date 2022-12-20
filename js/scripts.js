a54109c31ff25089c6114dfd83b9d59d946a1e38
Brandon Vasquez <thatsjaskett@gmail.com>
2022-12-20 16:40:24 +0100

    Fine Milestone 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,53 @@
-let submit = document.getElementById('generate_ticket');
+const submit = document.getElementById('generate');
+const refresh = document.getElementById('refresh');
 
 submit.addEventListener('click',
     function() {
+        document.getElementById('generate').innerHTML = "Rigenera";
+
+        // Dati passeggero
         let name_passenger = document.getElementById('name').value;
         console.log(`L'utente si chiama ${name_passenger}`);
+
+        // Distanza da percorrere
         let distance = document.getElementById('distance').value;
         console.log(`L'utente deve percorrere ${distance}Km`);
+
+        // Fascia d'età del passeggero
         let age = document.getElementById('age').value;
         console.log(`L'utente rientra nella categoria sconto ${age}`);
+
+        // In caso di errore
+        if(isNaN(distance)) {
+            alert("Il valore inserito non è un numero! Per favore, inserire correttamente i valori richiesti");
+            location.reload();
+        }
+
+        // Calcolo prezzi biglietti
+        const km_price = 0.21;
+        const full_price_ticket = distance * km_price;
+        const ticket = full_price_ticket.toFixed(2);
+        console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
+
+        
+        // Calcolo sconto Under18
+        let ticket_under = full_price_ticket * 0.80;
+        let ticket_d_under = ticket_under.toFixed(2);
+        console.log(`Il prezzo del biglietto per Under 18 è: ${ticket_d_under}€`);
+
+        // Calcolo sconto Over65
+        let ticket_over = full_price_ticket * 0.60;
+        let ticket_d_over = ticket_over.toFixed(2);
+        console.log(`Il prezzo del biglietto per Over 65 è: ${ticket_d_over}€`);
     }    
 );
 
-// if(isNaN(age) || isNaN(distance)) {
-//     alert("Il valore inserito non è un numero! Per favore, inserire correttamente i valori richiesti");
-//     location.reload();
-// }
-
-// // Calcolo prezzi biglietti
-// const km_price = 0.21;
-// const full_price_ticket = distance * km_price;
-// const ticket = full_price_ticket.toFixed(2);
-// console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
-
-// // Calcolo sconto Under18
-// let ticket_under = full_price_ticket * 0.80;
-// let ticket_d_under = ticket_under.toFixed(2);
-// console.log(`Il prezzo del biglietto per Under 18 è: ${ticket_d_under}€`);
-
-// // Calcolo sconto Over65
-// let ticket_over = full_price_ticket * 0.60;
-// let ticket_d_over = ticket_over.toFixed(2);
-// console.log(`Il prezzo del biglietto per Over 65 è: ${ticket_d_over}€`);
+refresh.addEventListener('click',
+    function() {
+        console.log('Aggiorno la pagina');
+        location.reload();
+    }
+)
 
 // // Output prezzo biglietto del treno con sconti
 // if(age < 18) {
